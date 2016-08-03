a0419939b22e1a8b8a621df89c2631c78c090296
Kai <zengkai2009@foxmail.com>
2016-08-03 15:31:22 +0800

    server: add utils module

---
+++ server/test/utils.js
@@ -0,0 +1,42 @@
+import Msg from '../src/config/message'
+
+export const user = {
+  nickname: 'kai',
+  email: '489272441@qq.com',
+  password: '1234567'
+}
+
+export const message = {
+  content: 'test'
+}
+
+export function signin (cb) {
+  request(app)
+    .post('/api/user')
+    .send(user)
+    .expect(200)
+    .end((err, res) => {
+      expect(err).to.be.not.ok
+
+      expect(res).to.be.a('object')
+      const token = res.body.data.token
+      expect(token).to.be.a('string')
+      expect(token).to.have.lengthOf(172)
+      cb(token)
+    })
+}
+
+export function createMessage (token, cb) {
+  request(app)
+    .post('/api/message')
+    .set('Authorization', `Bearer ${token}`)
+    .send(message)
+    .expect(200)
+    .end((err, res) => {
+      expect(err).to.be.not.ok
+      expect(res).to.be.a('object')
+      expect(res.body.code).to.equal(Msg.noObjectId.code)
+
+      cb(res)
+    })
+}
