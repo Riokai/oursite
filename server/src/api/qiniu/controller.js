2253ca79059726b79aadfdacd07c520ba37cbd53
Kai <zengkai2009@foxmail.com>
2016-11-09 15:49:22 +0800

    optimize qiniu env

--- server/src/api/qiniu/controller.js
+++ server/src/api/qiniu/controller.js
@@ -5,9 +5,10 @@ import config from '../../config/qiniu'
 qiniu.conf.ACCESS_KEY = config.AccessKey
 qiniu.conf.SECRET_KEY = config.SecretKey
 
-const upToken = new qiniu.rs.PutPolicy(config.bucket)
 
 export function query (req, res) {
+  const bucket = req.query.env === 'dev' ? config.bucketDev : config.bucketProd
+  const upToken = new qiniu.rs.PutPolicy(bucket)
   const token = upToken.token()
 
   res.header("Cache-Control", "max-age=0, private, must-revalidate")
